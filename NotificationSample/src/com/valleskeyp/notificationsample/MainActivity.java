85811468b886b0aa4a2525a8d8d428ceaaa8d490
Peter Valleskey <player1122@gmail.com>
2014-01-14 15:18:03 -0800

    Progress update
    Few hours of work and research into the project.  Have a clear idea of
    what will follow from this point.
    Implement an interface for the return of date and time data.  When that
    is completed, make use of AlarmManager to fire the notifications at the
    appropriate time.

--- NotificationSample/src/com/valleskeyp/notificationsample/MainActivity.java
+++ NotificationSample/src/com/valleskeyp/notificationsample/MainActivity.java
@@ -1,23 +1,85 @@
 package com.valleskeyp.notificationsample;
 
+import com.valleskeyp.pickers.DatePickerFragment;
+import com.valleskeyp.pickers.TimePickerFragment;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.DialogFragment;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
+	EditText eventInfo;
+	TextView dateField;
+	TextView timeField;
+	
+	Button pickDateButton;
+	Button pickTimeButton;
+	Button submitButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        
+        eventInfo = (EditText)findViewById(R.id.eventInfo);
+        dateField = (TextView)findViewById(R.id.dateField);
+        timeField = (TextView)findViewById(R.id.timeField);
+        
+        pickDateButton = (Button)findViewById(R.id.selectDate);
+        pickTimeButton = (Button)findViewById(R.id.selectTime);
+        submitButton = (Button)findViewById(R.id.submit);
+        
+        pickDateButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				DialogFragment dateFragment = new DatePickerFragment();
+        	    dateFragment.show(getFragmentManager(), "datePicker");
+			}
+        });
 
+        pickTimeButton.setOnClickListener(new OnClickListener() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        	@Override
+        	public void onClick(View v) {
+        		DialogFragment timeFragment = new TimePickerFragment();
+        		timeFragment.show(getFragmentManager(), "timePicker");
+        	}
+        });
+
+        submitButton.setOnClickListener(new OnClickListener() {
+
+        	@Override
+        	public void onClick(View v) {
+        		if (eventInfo.length() > 0) {
+					if (dateField.length() > 0) {
+						if (timeField.length() > 0) {
+							Toast.makeText(getApplicationContext(),
+									"Submitted event", Toast.LENGTH_SHORT)
+									.show();
+							eventInfo.setText("");
+							dateField.setText("");
+							timeField.setText("");
+						} else {
+							Toast.makeText(getApplicationContext(), "Please select a time",
+									Toast.LENGTH_SHORT).show();
+						}
+					} else {
+						Toast.makeText(getApplicationContext(), "Please select a date",
+								Toast.LENGTH_SHORT).show();
+					}
+				} else {
+					Toast.makeText(getApplicationContext(), "Please input event info",
+							Toast.LENGTH_SHORT).show();
+				}
+        	}
+        });
     }
     
 }
